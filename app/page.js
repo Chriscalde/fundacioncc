ceb6ab9e8328d11862e5b35b8759d39304267ccd
Chris <chris.calderon.ortega@gmail.com>
2024-05-23 13:07:55 -0600

    Update

--- app/page.js
+++ app/page.js
@@ -3,17 +3,18 @@ import Image from "next/image";
 import logoCC from "../assets/img/logocc.png" 
 import fbIcon from "../assets/svg/facebook1.svg"
 import waIcon from "../assets/svg/whatsapp1.svg"
-import style from "../styles/Home.module.css"
+import styles from "../styles/Home.module.css"
+import rLogo from "../assets/svg/raptorslogo.svg"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="flex flex-wrap bg-secondary w-full p-5">
+    <main className="flex h-screen flex-col items-center justify-normal bg-primary p-0">
+      <div className="flex flex-wrap bg-secondary w-full p-2">
         <div className="mr-auto">
           <Image src={logoCC} width={100} height={100}/>
         </div>
         <div className="my-auto flex flex-row">
-          <a href="https://www.facebook.com/profile.php?id=100044643034540">
+          <a href="https://www.facebook.com/profile.php?id=100044643034540" target="_blank" rel="noopener noreferrer">
             <Image src={fbIcon} color="#DCDCDD" width={40} height={40} className="mx-5"/>
           </a>
           <a href="tel:+526394650034">
@@ -21,15 +22,21 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className={style.maindiv} >
+      <div className={styles.maindiv} >
           <div className="text-center my-auto">
-            <h1 className="text-secondary text-6xl font-bold">Próximamente</h1>
-            <h2 className="text-terciary text-2xl font-semibold italic">¡Algo Grande Está en Camino!</h2>
+            <h1 className="text-secondary md:text-6xl text-5xl font-bold">Próximamente</h1>
+            <h2 className="text-primary md:text-2xl text-xl font-semibold italic">¡Algo Grande Está en Camino!</h2>
           </div>
       </div>
-      <div className="bg-secondary flex flex-wrap h-10 w-full text-center justify-center items-center">
-        <a className="text-primary text-center mx-auto my-3"> &copy; Fundación Carlos Cuevas 2024 | Desarrollado por <span>Raptor Devs</span></a>
-      </div>
+      <footer className="bg-secondary md:flex md:flex-row w-full text-center justify-center items-center align-middle py-4 md:py-2 bottom-0 h-full">
+        <a href="https://raptordevs.com" target="_blank" rel="noopener noreferrer" className="text-primary mx-auto flex flex-grow justify-center items-center">
+          <span className="mr-2">&copy; Fundación Carlos Cuevas | </span>
+          Powered by {' '}
+          <span className=" ">
+            <Image src={rLogo} alt="logo" width={80} height={60}></Image>
+          </span>
+        </a>
+      </footer>
     </main>
   );
 }
