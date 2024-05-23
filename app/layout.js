2f26a142c6288b55958581251c9696b9bbe6e98c
Chris <chris.calderon.ortega@gmail.com>
2024-05-23 16:57:40 -0600

    Changed icon

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="./favicon.ico"></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
